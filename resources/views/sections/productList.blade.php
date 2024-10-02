f16a3b08b2af9bf5c7767a0a5d2404ea63fa537a
unknown <pillorajem10@gmail.com>
2024-10-02 13:35:02 +0800

    added versioning queries in asset links

--- resources/views/sections/productList.blade.php
+++ resources/views/sections/productList.blade.php
@@ -62,38 +62,61 @@
                 <ul class="product-section-pagination-list">
                     {{-- Previous Page Link --}}
                     @if ($products->onFirstPage())
-                        <li class="product-section-disabled"><span>&laquo; Previous</span></li>
+                        <li class="product-section-disabled"><span>Prev</span></li>
                     @else
                         <li>
                             <a class="product-section-link" 
-                               href="{{ $products->previousPageUrl() }}{{ $search ? '&search=' . $search : '' }}">&laquo; Previous</a>
+                            href="{{ $products->previousPageUrl() }}{{ $search ? '&search=' . $search : '' }}">Prev</a>
                         </li>
                     @endif
-        
+
                     {{-- Pagination Elements --}}
-                    @foreach ($products->getUrlRange(1, $products->lastPage()) as $page => $url)
-                        @if ($page == $products->currentPage())
-                            <li class="product-section-active"><span>{{ $page }}</span></li>
-                        @else
-                            <li>
-                                <a class="product-section-link" 
-                                   href="{{ $url }}{{ $search ? '&search=' . $search : '' }}">{{ $page }}</a>
-                            </li>
-                        @endif
-                    @endforeach
-        
+                    @php
+                        $currentPage = $products->currentPage();
+                        $lastPage = $products->lastPage();
+                    @endphp
+
+                    {{-- Show first page --}}
+                    <li>
+                        <a class="product-section-link" href="{{ $products->url(1) }}{{ $search ? '&search=' . $search : '' }}">1</a>
+                    </li>
+
+                    {{-- Show ellipsis if needed --}}
+                    @if ($currentPage > 3)
+                        <li class="product-section-ellipsis"><span>...</span></li>
+                    @endif
+
+                    {{-- Show pages around the current page --}}
+                    @for ($i = max(2, $currentPage - 1); $i <= min($lastPage - 1, $currentPage + 1); $i++)
+                        <li class="{{ $i == $currentPage ? 'product-section-active' : '' }}">
+                            <a class="product-section-link" href="{{ $products->url($i) }}{{ $search ? '&search=' . $search : '' }}">{{ $i }}</a>
+                        </li>
+                    @endfor
+
+                    {{-- Show ellipsis if needed --}}
+                    @if ($currentPage < $lastPage - 2)
+                        <li class="product-section-ellipsis"><span>...</span></li>
+                    @endif
+
+                    {{-- Show last page --}}
+                    @if ($lastPage > 1)
+                        <li>
+                            <a class="product-section-link" href="{{ $products->url($lastPage) }}{{ $search ? '&search=' . $search : '' }}">{{ $lastPage }}</a>
+                        </li>
+                    @endif
+
                     {{-- Next Page Link --}}
                     @if ($products->hasMorePages())
                         <li>
                             <a class="product-section-link" 
-                               href="{{ $products->nextPageUrl() }}{{ $search ? '&search=' . $search : '' }}">Next &raquo;</a>
+                            href="{{ $products->nextPageUrl() }}{{ $search ? '&search=' . $search : '' }}">Next</a>
                         </li>
                     @else
-                        <li class="product-section-disabled"><span>Next &raquo;</span></li>
+                        <li class="product-section-disabled"><span>Next</span></li>
                     @endif
                 </ul>
             @endif
-        </div>               
+        </div>
     </div>
 </body>
 </html>
